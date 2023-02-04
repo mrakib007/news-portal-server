9dd415a0ee3ccceec2ad6fd610551bc2d9f0ef81
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-02-04 14:45:23 +0600

    backend code done

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(cors());
 
-const categories = require('./data/categories.json');
-const news = require('./data/news.json');
+const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 
-app.get('/',(req,res)=>{
-    res.send('News API Running');
+app.get("/", (req, res) => {
+  res.send("News API Running");
 });
-app.get('/news-categories',(req,res)=>{
-    res.send(categories);
-}),
-app.get('/news/:id',(req,res)=>{
-    const id = req.params.id;
-    if(id ==='08'){
-        res.send(news);
-    }
-    else{
-        const selectedNews = news.find(n => n._id === id);
-        res.send(selectedNews);
-    }
+app.get("/news-categories", (req, res) => {
+  res.send(categories);
 });
-app.get('/category/:id',(req,res)=>{
+  app.get("/news/:id", (req, res) => {
     const id = req.params.id;
-    const category_news = news.filter(n=>n.category_id === id);
-    res.send(category_news); 
-})
-app.get('/news',(req,res)=>{
+    const selectedNews = news.find((n) => n._id === id);
+    res.send(selectedNews);
+  });
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "08") {
     res.send(news);
-})
-app.listen(port,()=>{
-    console.log('News Portal Server Running on port 5000');
+  } else {
+    const category_news = news.filter((n) => n.category_id === id);
+    res.send(category_news);
+  }
+});
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+app.listen(port, () => {
+  console.log("News Portal Server Running on port 5000");
 });
